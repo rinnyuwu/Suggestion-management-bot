a6aa9aea3205bb46fd557fff2ad4ba44371f9cbf
rinnyuwu <catlandpro@gmail.com>
2024-12-12 22:12:21 +0100

    Add config

---
+++ config/config.py
@@ -0,0 +1,6 @@
+class Config:
+    # Channel ID where suggestions will be sent
+    SUGGESTIONS_CHANNEL_ID = 123456789  # Replace with actual channel ID
+
+    # Roles that can change the suggestion status
+    ADMIN_ROLES = [123456789, 123456789]  # Replace with actual role IDs
